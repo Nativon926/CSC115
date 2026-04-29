4f289f5ee01c4e5918c83e9bca19f9203e4c0aa0
Nativon926 <39961225+Nativon926@users.noreply.github.com>
2026-04-28 20:52:56 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * Miranda Germain
+ * 8/30/2022
+ * CSC 115
+ * HelloWorld.java
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+public class HelloWorld {
+public static void pickNumber() {
+   String password = "PASSWORD";
+   System.out.println(password.substring(0,1).toLowerCase());	
+}}
