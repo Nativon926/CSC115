c4556e9ccf1f37514a32b8af8fe25b8934000de7
Nativon926 <39961225+Nativon926@users.noreply.github.com>
2026-04-29 08:26:55 -0400

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+/**
+ * Filename: Rentangle.java
+ * Author: Miranda Germain
+ * Date: 9/8/2022
+ * CSC 115
+ */
+
+public class Rectangle {
+   public static void main(String[] args){
+      printRectangle();
+   }
+
+   public static void printRectangle(){
+    // Print a rectangle made of astericks
+      System.out.println("*****");
+      System.out.println("*   *");
+      System.out.println("*   *");
+      System.out.print("*****");
+   }
+}
