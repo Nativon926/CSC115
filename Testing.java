d01cd8d63ec9cbeec4a8d64c75ea42e25d113687
Nativon926 <39961225+Nativon926@users.noreply.github.com>
2026-04-28 21:12:28 -0400

    Add files via upload

--- Testing.java
+++ Testing.java
@@ -2,9 +2,32 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Testing {
-public static void main() {
-   String str1 = "Froda Baggins";
-   System.out.println(str1.toLowerCase().indexOf("B"));
+public static void main(String[] args) {
+
+
+int grade = 84;
+
+if(grade >= 100) {
+
+  System.out.println("You got a perfect score!");
+
+} else if(grade >= 90) {
+
+  System.out.println("You did really well!");
+
+} else if(grade >= 80) {
+
+  System.out.println("Solid!");
+
+} else if(grade >= 70) {
+
+  System.out.println("Room for improvement!");
+
+} else {
+
+  System.out.println("You need some help with this!");
+
+}
 
 }
 }
