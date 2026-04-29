5e5cd1309d6d36e097d7287007f3ee1a836ff9aa
Nativon926 <39961225+Nativon926@users.noreply.github.com>
2026-04-29 08:29:21 -0400

    Add files via upload

---
+++ Sum3ConsecutiveNumbers.java
@@ -0,0 +1,48 @@
+/**
+ * Take integer input from the user and calculate the sum of user's number and
+ * the next 2 consecutive numbers. Then output that answer to the console
+ *
+ * @author Miranda Germain
+ * @since 9/8/22
+ */
+import java.util.Scanner;
+
+public class Sum3ConsecutiveNumbers {
+  public static void main(String[] args) {
+    callTheFunctionToDoTheWork();
+  }
+
+  public static void callTheFunctionToDoTheWork() {
+
+int numberOfFriends = 7;
+
+if(numberOfFriends > 5) {
+  int numberOfCars = 2;
+} else {
+  int numberOfCars = 1;
+}
+
+System.out.println("We will need to take " + numberOfCars + " cars.");
+
+}
+
+}
+    // ===========================================
+
+// =============== LAB SUMMARY ===============
+
+/*
+ * 1. How much time was required to complete this assignment?
+ * An hour at most. I spent a lot of time checking the format.
+ * Writing the actual code probably took 20 minutes since I had to figure out
+ * specifically what the lab wanted me to do.
+ *
+ * 2. What was the hardest part of this lab?
+ * Trying to find the formatting error. And it took a moment to figure out
+ * how to write the code for the second string with math and concatenations.
+ *
+ * 3. What will you always remember (never forget) from this exercise?
+ * How important parenthesis are, and not to overthink things since starting
+ * out I imagined this super complicated expression to find the sum when
+ * really it could be found with simple arithmetic.
+ */
