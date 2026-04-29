3b845e331b1d64cdc08189457b24a016415f4cdd
Nativon926 <39961225+Nativon926@users.noreply.github.com>
2026-04-28 20:50:59 -0400

    Add files via upload

---
+++ First.java
@@ -0,0 +1,15 @@
+/*
+ * Miranda Germain
+ * 8/30/2022
+ * CSC 115
+ * HelloWorld.Java
+ */
+
+public class First {
+   public static void main(String[] args) {
+      // display output
+      System.out.print("Hello, World!");
+      String hello = new String(" Hello World!!!");
+      System.out.print(hello);
+   }
+}
