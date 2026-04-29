a9d9b1363b279ee32cf473ecca8a08e74999a3d5
Nativon926 <39961225+Nativon926@users.noreply.github.com>
2026-04-28 21:06:32 -0400

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Testing {
+public static void main() {
+   String str1 = "Froda Baggins";
+   System.out.println(str1.toLowerCase().indexOf("B"));
+
+}
+}
